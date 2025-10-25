d4ce5aadfd5ff4bcb6219c18ea3c218df73d3152
Rustam Mullaev <sportandyoung@gmail.com>
2025-10-25 20:02:13 +0500

    new project

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+      <header className='header'>
+        <img className='header-logo' src="https://png.pngtree.com/png-vector/20210413/ourmid/pngtree-business-company-logo-design-png-image_3204914.jpg" alt='logo' />
+      </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,16 @@
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+            <img src='https://dynamic-media-cdn.tripadvisor.com/media/photo-o/15/17/7f/a5/big-beach.jpg?w=900&h=500&s=1' alt='bg-photo' />
+            </div>
+            <div>
+                <img width={100} src='https://cms.bps.org.uk/sites/default/files/2022-09/Grumpy%20cat%202.jpeg' alt='avatar' />
+            </div>
+            <div></div>
+        </div>
+    )
+}
+
+export default Profile
+
